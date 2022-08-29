125f16a8890160672c5b9698a70abd774f366914
amirshd22 <amirshd2295@gmail.com>
2022-08-29 10:55:04 +0430

    changed the time to 20th of september

--- src/components/Home/Timer.tsx
+++ src/components/Home/Timer.tsx
@@ -3,15 +3,15 @@ import Card from "react-bootstrap/Card";
 import Countdown from "react-countdown";
 
 const Timer: React.FC = () => {
-  const august = 36 * 24 * 60 * 60 * 1000;
+  const september = 22 * 24 * 60 * 60 * 1000;
 
-  const date = 1658834871851 + august + 19800000;
+  const date = 1661753987000 + september + 19800000;
   return (
     <Card className="mb-2 text-center">
       <Card.Body>
         <p>
-          Vader will be listed on 31 August on pancake swap and be available for
-          trading!
+          Vader will be listed on 20 September on pancake swap and be available
+          for trading!
         </p>
         <h1 className="text-primary">
           <Countdown date={date} />
